652f7e93d3a8e766cec8eaf24b443bd87735a50a
minh.vu <minh.vu@sotatek.com>
2021-11-24 15:40:50 +0700

    create contract erc721

---
+++ contracts/MtokenERC721.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../token/ERC721/ERC721.sol";
+import "../token/utils/Counters.sol";
+
+contract MtokenErc721 is ERC721 {
+
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor (
+      string memory name_,
+      string memory symbol_
+  ) ERC721(name_, symbol_) {}
+
+  function mint(address _to) public returns(uint256) {
+    _tokenIds.increment();
+
+    uint256 newTokenId = _tokenIds.current();
+    _mint(_to, newTokenId);
+
+    return newTokenId;
+  }
+}
---
+++ scripts/deployERC721.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Mtoken721 = await hre.ethers.getContractFactory("MtokenErc721");
+  const mtoken721 = await Mtoken721.deploy("Mtoken721", "MT721");
+
+  await mtoken721.deployed();
+
+  console.log("Mtoken deployed to:", mtoken721.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
